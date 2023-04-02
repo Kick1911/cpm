af0a3bb243305a04acd65cd50ffdfef948c2b49d
Caswall Engelsman <mail@cengelsman.com>
2023-04-02 23:48:28 +0200

    Add -g3 on test

--- share/templates/tests_makefile.mk
+++ share/templates/tests_makefile.mk
@@ -15,7 +15,7 @@ ${TESTS_OUT}: %.out: %.c
 	${Q}${CC} $< -o $@ ${CFLAGS} ${LDFLAGS} ${APP_NAME:%=-l%}
 
 shared_library:
-	${Q}${MAKE} -C .. shared_library
+	${Q}DEBUG=-g3 ${MAKE} -C .. shared_library
 
 test: all
 	@for exe in $(TESTS_OUT) ; do \
