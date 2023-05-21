fb26ed06249b3c0d54197e1611e32fd70842525e
Caswall Engelsman <mail@cengelsman.com>
2023-05-21 22:36:25 +0200

    Fix test Makefile

--- share/templates/tests_makefile.mk
+++ share/templates/tests_makefile.mk
@@ -20,7 +20,7 @@ shared_library:
 test: all
 	@for exe in $(TESTS_OUT) ; do \
 		LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${shell pwd}/.. valgrind --error-exitcode=1 --leak-check=full $$exe; \
-		if [ $? != 0 ]; then return 1; fi \
+		if [ $$? -ne 0 ]; then return 1; fi; \
 	done
 
 clean:
