efcf570a6a86dc3dda68288f0a58f2129180da8d
Caswall Engelsman <mail@cengelsman.com>
2024-02-27 00:52:13 +0200

    Use non-deprecated interface for unitest.h

--- tests/test_render.c
+++ tests/test_render.c
@@ -4,39 +4,48 @@
 #include <malloc.h>
 #include <string.h>
 
-int main(void){
-    TEST(Rendering start,
-        char template[] = "$0<a href=\"\">Blah$1</a>";
-        char* args[] = {"/kick"};
-        size_t len = render_length(template, (const char**)args, sizeof(args)/sizeof(char*));
-        char* res = render(template, (const char**)args, sizeof(args)/sizeof(char*));
+void
+rendering_start() {
+    char template[] = "$0<a href=\"\">Blah$1</a>";
+    char* args[] = {"/kick"};
+    size_t len = render_length(template, (const char**)args, sizeof(args)/sizeof(char*));
+    char* res = render(template, (const char**)args, sizeof(args)/sizeof(char*));
 
-        T_ASSERT_NUM(len, 30);
-        T_ASSERT_STRING(res, "/kick<a href=\"\">Blah(null)</a>");
-        free(res);
-    );
-
-    TEST(Rendering mid,
-        char template[] = "<a href=\"$0\">$1Blah$$#0</a>";
-        char* args[] = {"/kick"};
-        size_t len = render_length(template, (const char**)args, sizeof(args)/sizeof(char*));
-        char* res = render(template, (const char**)args, sizeof(args)/sizeof(char*));
-
-        T_ASSERT_NUM(len, 32);
-        T_ASSERT_STRING(res, "<a href=\"/kick\">(null)Blah$0</a>");
-        free(res);
-    );
-
-    TEST(Rendering end,
-        char template[] = "<a href=\"\">Blah$1</a>$0";
-        char* args[] = {"/kick"};
-        size_t len = render_length(template, (const char**)args, sizeof(args)/sizeof(char*));
-        char* res = render(template, (const char**)args, sizeof(args)/sizeof(char*));
-
-        T_ASSERT_NUM(len, 30);
-        T_ASSERT_STRING(res, "<a href=\"\">Blah(null)</a>/kick");
-        free(res);
-    );
-    return 0;
+    T_ASSERT_NUM(len, 30);
+    T_ASSERT_STRING(res, "/kick<a href=\"\">Blah(null)</a>");
+    free(res);
 }
 
+void
+rendering_mid() {
+    char template[] = "<a href=\"$0\">$1Blah$$#0</a>";
+    char* args[] = {"/kick"};
+    size_t len = render_length(template, (const char**)args, sizeof(args)/sizeof(char*));
+    char* res = render(template, (const char**)args, sizeof(args)/sizeof(char*));
+
+    T_ASSERT_NUM(len, 32);
+    T_ASSERT_STRING(res, "<a href=\"/kick\">(null)Blah$0</a>");
+    free(res);
+}
+
+void
+rendering_end() {
+    char template[] = "<a href=\"\">Blah$1</a>$0";
+    char* args[] = {"/kick"};
+    size_t len = render_length(template, (const char**)args, sizeof(args)/sizeof(char*));
+    char* res = render(template, (const char**)args, sizeof(args)/sizeof(char*));
+
+    T_ASSERT_NUM(len, 30);
+    T_ASSERT_STRING(res, "<a href=\"\">Blah(null)</a>/kick");
+    free(res);
+}
+
+int
+main(void) {
+    TEST(Rendering start, rendering_start());
+
+    TEST(Rendering mid, rendering_mid());
+
+    TEST(Rendering end, rendering_end());
+    return 0;
+}
