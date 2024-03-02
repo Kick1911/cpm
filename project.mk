ce14b1feff66a32d7b9bd1dbbb47391450de4e62
Caswall Engelsman <mail@cengelsman.com>
2024-03-02 18:50:12 +0200

    Remove unused library and Makefile code

--- project.mk
+++ project.mk
@@ -1,23 +1,21 @@
 APP_NAME = cpm
 VERSION = 0.14.2
-PACKAGE_CONTENTS := ${APP_NAME} ${SHARE_PATH}
+PACKAGE_CONTENTS := ${APP_NAME}
 
 PROD_CFLAGS = -O3 -static
 INSTALL_PATH = /opt
 CFLAGS += -DINSTALL_PATH=${INSTALL_PATH}
 
 # Projects
-JSON_PROJECT_ID = 16394907
 UNITEST_PROJECT_ID = 11728468
 
-STATIC_DEP = json
+STATIC_DEP =
 SHARED_DEP = c
 
-DEPENDENCIES += gitlab/${JSON_PROJECT_ID}/0.0.2
 DEPENDENCIES += gitlab/${UNITEST_PROJECT_ID}/0.2.0
 
 LDFLAGS += ${STATIC_DEP:%=-l%} ${SHARED_DEP:%=-l%}
-INSTALL_STEPS = install_share_folder install_binary
+INSTALL_STEPS = install_binary
 
 project: all
 preprocess: ${SRC_PATH}/project_map.h
